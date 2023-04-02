ff5d60e692fde75299853fb75d5bc01232754761
Biraj <birajkarki9849@gmail.com>
2023-04-02 23:37:25 +0545

    connection established with firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIQot6n0ekyA426eV7OHdAt_wAzV-pVaE",
+  authDomain: "learn-firebase-9395c.firebaseapp.com",
+  projectId: "learn-firebase-9395c",
+  storageBucket: "learn-firebase-9395c.appspot.com",
+  messagingSenderId: "179396143235",
+  appId: "1:179396143235:web:084e4b5732228167aa53b6",
+  measurementId: "G-60JZLNJHFD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
